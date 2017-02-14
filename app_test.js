e7af6cff6265e27b8d8e170fb8afa893429d918e
wk-regulus <keyaruchu@gmail.com>
2017-02-14 22:46:30 +0900

    first commit

---
+++ app_test.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+
+// HTTPリクエストを受け取る部分
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// サーバーを起動する部分
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+//コメント
